81226806aa1f7ae23d56af76eed5c7f17ae2d0a1
Keenan Nemetz <keenan.nemetz@gmail.com>
2022-04-06 15:09:52 -0700

    add missing paymaster deploy script steps

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,6 +3,7 @@ import { ethers } from 'hardhat';
 async function main() {
   const { chainId } = await ethers.provider.getNetwork();
   const forwarderAddress = getForwarderAddress(chainId);
+  const relayHubAddress = getRelayHubAddress(chainId);
 
   console.log("Deploying to:", chainId);
 
@@ -19,12 +20,29 @@ async function main() {
   const paymaster = await Paymaster.deploy();
   await paymaster.deployed();
 
+  const setRelayHubTx = await paymaster.setRelayHub(relayHubAddress);
+  await setRelayHubTx.wait();
+
+  const setTrustedForwarderTx = await paymaster.setTrustedForwarder(forwarderAddress);
+  await setTrustedForwarderTx.wait();
+
   const allowAddressTx = await paymaster.allowAddress(registry.address);
   await allowAddressTx.wait();
 
   console.log("Registry deployed to:", registry.address);
   console.log("License deployed to:", license.address);
   console.log("Paymaster deployed to:", paymaster.address);
+}
+
+function getRelayHubAddress(chainId: number): string {
+  switch(chainId) {
+    case 137: // polygon mainnet
+      return '0x6C28AfC105e65782D9Ea6F2cA68df84C9e7d750d';
+    case 80001: // polygon mumbai
+      return '0x6646cD15d33cE3a6933e36de38990121e8ba2806';
+    default: // testnet or unknown
+      return '0x0000000000000000000000000000000000000000';
+  }
 }
 
 function getForwarderAddress(chainId: number): string {
